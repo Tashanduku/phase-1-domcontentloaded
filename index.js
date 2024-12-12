4e632ac0e80421f17a683713c0b45ace71f1090d
Natasha Nduku <natashanduku0@gmail.com>
2024-12-12 09:56:18 +0300

    document loaded lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const paragraph = document.getElementById("text");
+  
+  
+    paragraph.textContent = "This is really cool!";
+  
+    
+    console.log("Page loaded and text updated successfully.");
+  });
+  
